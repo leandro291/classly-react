71de2f156fb2a4766289a485f7ad164189de6906
lenadro291 <lenadrito1@gmail.com>
2026-08-12 21:04:34 -0500

    add pages export for LoginPage, Dashboard, and CourseDetail components

---
+++ src/app/pages.jsx
@@ -0,0 +1,3 @@
+export { default as LoginPage } from '../features/pages/auth/pages/LoginPage'
+export { default as Dashboard } from '../features/pages/courses/pages/Dashboard'
+export { default as CourseDetail } from '../features/pages/courses/pages/CourseDetail'
