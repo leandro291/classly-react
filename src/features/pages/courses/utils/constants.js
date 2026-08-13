74eb82e404b6aad782d59d2b6d664fb5889fd525
lenadro291 <lenadrito1@gmail.com>
2026-08-12 21:02:18 -0500

    add constants for card and avatar colors

---
+++ src/features/pages/courses/utils/constants.js
@@ -0,0 +1,6 @@
+export const CARD_COLORS = [
+  '#1E8E3E', '#1A73E8', '#E8710A', '#7627BB',
+  '#00897B', '#D93025', '#F29900', '#E52592',
+]
+
+export const AVATAR_COLORS = ['#1a73e8', '#1e8e3e', '#7627bb', '#e8710a', '#00897b', '#d93025', '#f29900']
