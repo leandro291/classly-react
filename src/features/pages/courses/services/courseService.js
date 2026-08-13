2965e197c1ca6e6463b16d20c83742b1da2b9c0a
lenadro291 <lenadrito1@gmail.com>
2026-08-12 21:03:06 -0500

    add course service with CRUD operations for courses, materials, tasks, and submissions

---
+++ src/features/pages/courses/services/courseService.js
@@ -0,0 +1,13 @@
+import {
+  getCourses, createCourse, updateCourse, deleteCourse, joinCourse,
+  getMaterials, createMaterial, updateMaterial, deleteMaterial,
+  getTareas, createTarea, updateTarea, deleteTarea,
+  getEntregas, createEntrega, updateEntrega, deleteEntrega, gradeEntrega,
+} from '../../../../common/services/api'
+
+export {
+  getCourses, createCourse, updateCourse, deleteCourse, joinCourse,
+  getMaterials, createMaterial, updateMaterial, deleteMaterial,
+  getTareas, createTarea, updateTarea, deleteTarea,
+  getEntregas, createEntrega, updateEntrega, deleteEntrega, gradeEntrega,
+}
